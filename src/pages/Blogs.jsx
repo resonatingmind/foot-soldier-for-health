37e1ebcc54bebe834485c39435408e110a838bd9
resonatingmind <adityakumarsince2004@kgpian.iitkgp.ac.inclear>
2024-08-10 18:16:31 +0530

    Adding Blogs Section

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import PageTop from '../components/pageTop'
+import SingleBlog from './SingleBlog'
 
 export default function Blogs() {
   return (
     <div>
     <Header/>
-    <PageTop title="Blogs" breadcrum="Home > Blogs"/>
+    <PageTop title="News And Blogs" breadcrum="Home > Blogs"/>
+    <div className='newsAndBlogsCont sectionPadding'>
+        <div className='blogsCont'>
+           
+        </div>
+        <div className='newsAndCategoryCont'>
+            <div className='newsContainer'>
+                
+            </div>
+            <div className='categoryContainer'>
+               
+            </div>
+        </div>
+    </div>
     <Footer/>
     </div>
   )
---
+++ src/pages/SingleBlog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SingleBlog() {
+  return (
+    <div>
+      This is single blog
+    </div>
+  )
+}
